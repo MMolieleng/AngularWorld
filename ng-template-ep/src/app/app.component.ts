34b7b78670a8fdbcd090cc4502ace8f779060b1d
MMolieleng <mjmolieleng@gmail.com>
2023-06-14 23:24:57 +0200

    Added simple template example

---
+++ ng-template-ep/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ng-template-ep';
+
+  countries = [
+    { name: "Sudan", continent: "Africa" },
+    { name: "Angola", continent: "Africa" }]
+
+}
